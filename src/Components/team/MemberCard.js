a1a251a8824e801b588fa925bad427c90c5f1b04
RoLoDeXx <dexxrolo@gmail.com>
2020-02-05 14:23:50 +0530

    more pics added

--- src/Components/team/MemberCard.js
+++ src/Components/team/MemberCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Member= ({ img, name, position }) => {
+const Member = ({ img, name, position }) => {
+  console.log(`../../images/member_pic/${img}`);
   return (
     <div className="member-card" data-aos="zoom-out">
-      <img src={require(`../../images/cat2.gif`)} alt="profile" />
+      <img src={require(`../../images/member_pic/${img}`)} alt="profile" />
       <h3 className="dsc-font">{name}</h3>
       <p className="dsc-font">{position}</p>
     </div>
--- src/Components/team/Team.js
+++ src/Components/team/Team.js
@@ -3,7 +3,7 @@ import { Typography, Container, Box, Grid } from "@material-ui/core";
 import Navbar from "../common/Navbar";
 import { withRouter } from "react-router-dom";
 import Member from "./MemberCard";
-import profileIcon from "../../images/profileAvatar.jpg";
+// import profileIcon from "../../images/profileAvatar.jpg";
 // import Img from "../import_image/importImage.js";
 import {
   GroupWork,
@@ -16,145 +16,146 @@ import {
 import Section4 from "../landing-page/Section4";
 
 const Team = () => {
-
-    const coreArr=[
-      {
-        img:"",
-        name:"Fahad Siddiqui",
-        position:"Community Lead"
-      },
-      {
-      img:"",
-      name:"Shubham Dubey",
-      position:"Management Lead"
+  const coreArr = [
+    {
+      img: "FahadSiddiqui.jpg",
+      name: "Fahad Siddiqui",
+      position: "Community Lead"
     },
     {
-      img:"",
-      name:"Ayush Tiwari",
-      position:"Management Lead"
+      img: "ShubhamDubey.jpg",
+      name: "Shubham Dubey",
+      position: "Management Lead"
     },
     {
-      img:"SamarthSharma.jpg",
-      name:"Samarth Sharma",
-      position:"Technical Lead"
+      img: "AyushTiwari.jpg",
+      name: "Ayush Tiwari",
+      position: "Management Lead"
     },
     {
-      img:"ShreyanshVarshney.jpg",
-      name:"Shreyansh Varshney",
-      position:"Technical Advisior"
+      img: "SamarthSharma.jpg",
+      name: "Samarth Sharma",
+      position: "Technical Lead"
+    },
+    {
+      img: "ShreyanshVarshney.jpg",
+      name: "Shreyansh Varshney",
+      position: "Technical Advisior"
     }
-  ]
+  ];
 
-  const TechnicalArr=[
+  const TechnicalArr = [
     {
-      img:"",
-      name:"Jai Parakh",
-      position:"Full Stack"
+      img: "JaiParakh.jpg",
+      name: "Jai Parakh",
+      position: "Full Stack"
     },
     {
-    img:"",
-    name:"Rohan Verma",
-    position:"Android"
-  },
-  {
-    img:"",
-    name:"Abhinav Verma",
-    position:"ML"
-  },
-  {
-    img:"",
-    name:"Shivam Agarwal",
-    position:"ML"
-  },
-  {
-    img:"",
-    name:"Pawan Kumar",
-    position:"Web"
-  },
-  {
-    img:"",
-    name:"Satyam Mittal",
-    position:"ML"
-  },
-  {
-    img:"",
-    name:"Abhinav Srivastva",
-    position:"Web"
-  },
-  {
-    img:"",
-    name:"Simran Shilky",
-    position:"Web"
-  },
-  {
-    img:"",
-    name:"Utkarsh Pandey",
-    position:"Web"
-  },
-  {
-    img:"",
-    name:"Shriyam Tripathi",
-    position:"Flutter"
-  },
-  {
-    img:"",
-    name:"Suyash Singh",
-    position:"Web"
-  },
-  {
-    img:"",
-    name:"Aman Jhaketiya",
-    position:"ML"
-  },
-  {
-    img:"",
-    name:"Shubhangi Agarwaal",
-    position:"Web"
-  }
-]
-const managementTeam=[
-  {
-    img:"",
-    name:"Saksham Srivastav",
-    position:"Event Manager"
-  },
-  {
-    img:"",
-    name:"Aditya Dahiya",
-    position:"PR Head"
-  },
+      img: "RohanVerma.jpg",
+      name: "Rohan Verma",
+      position: "Android"
+    },
+    {
+      img: "AbhinavVerma.jpg",
+      name: "Abhinav Verma",
+      position: "ML"
+    },
+    {
+      img: "ShivamAggarwal.jpg",
+      name: "Shivam Agarwal",
+      position: "ML"
+    },
+    {
+      img: "PawanKumar.jpg",
+      name: "Pawan Kumar",
+      position: "Web"
+    },
+    {
+      img: "SatyamMittal.jpg",
+      name: "Satyam Mittal",
+      position: "ML"
+    },
+    {
+      img: "AbhinavSrivastava.jpg",
+      name: "Abhinav Srivastava",
+      position: "Web"
+    },
+    {
+      img: "SimranShilky.jpg",
+      name: "Simran Shilky",
+      position: "Web"
+    },
+    {
+      img: "UtkarshPandey.jpg",
+      name: "Utkarsh Pandey",
+      position: "Web"
+    },
+    {
+      img: "ShriyamTripathi.jpg",
+      name: "Shriyam Tripathi",
+      position: "Web"
+    },
+    {
+      img: "SuyashSingh.jpg",
+      name: "Suyash Singh",
+      position: "Web"
+    },
+    {
+      img: "AmanJakhetiya.jpg",
+      name: "Aman Jakhetiya",
+      position: "ML"
+    }
+  ];
+  const managementTeam = [
+    {
+      img: "SakshamSrivastava.jpg",
+      name: "Saksham Srivastava",
+      position: "Event Manager"
+    },
+    {
+      img: "cat2.gif",
+      name: "Aditya Dahiya",
+      position: "PR Head"
+    },
+    {
+      img: "cat2.gif",
+      name: "Megha Singhal",
+      position: "PR Member"
+    }
+  ];
+  const socialMedia = [
+    {
+      img: "TanishaSrivastava.jpg",
+      name: "Tanisha Srivastava",
+      position: "Social Media Head"
+    }
+  ];
+  const photoTeam = [
+    {
+      img: "cat2.gif",
+      name: "Yash",
+      position: "Photography"
+    },
+    {
+      img: "cat2.gif",
+      name: "Saksham",
+      position: "Photography"
+    }
+  ];
 
-  {
-    img:"",
-    name:"Surbhi Varshney",
-    position:"Content Writing"
-  },
-  {
-    img:"",
-    name:"Megha Singhal",
-    position:"PR Member"
-  }
+  const contentTeam = [
+    {
+      img: "ShubhangiAgarwal.jpg",
+      name: "Shubhangi Agarwal",
+      position: "Content"
+    },
+    {
+      img: "cat2.gif",
+      name: "Surbhi Varshney",
+      position: "Content"
+    }
+  ];
 
-]
-const socialMedia=[
-  {
-    img:"",
-    name:"Tanisha Srivastava",
-    position:"Social Media Head"
-  }
-]
-const photoTeam=[
-  {
-    img:"",
-    name:"Yash",
-    position:"Photography"
-  },
-  {
-    img:"",
-    name:"Saksham",
-    position:"Photography"
-  }
-]
   return (
     <div>
       <Navbar></Navbar>
@@ -177,45 +178,9 @@ const photoTeam=[
             justify="space-around"
             alignItems="center"
           >
-            {[1].map(val => (
+            {coreArr.map(val => (
               <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Fahad Siddiqui"
-                  position="DSC Member"
-                />
-              </Grid>
-            ))}
-            {[2].map(val => (
-              <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Shubham"
-                  position="DSC Member"
-                />
-              </Grid>
-            ))}
-            {[3].map(val => (
-              <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Shriyam"
-                  position="DSC Member"
-                />
-              </Grid>
-            ))}
-            {[4].map(val => (
-              <Grid>
-                <Member img={profileIcon} name="Uphaar" position="DSC Member" />
-              </Grid>
-            ))}
-            {[7].map(val => (
-              <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Ayush Tiwari"
-                  position="DSC Member"
-                />
+                <Member img={val.img} name={val.name} position={val.position} />
               </Grid>
             ))}
           </Grid>
@@ -239,13 +204,9 @@ const photoTeam=[
             justify="space-around"
             alignItems="center"
           >
-            {[1, 2, 7, 9].map(val => (
+            {TechnicalArr.map(val => (
               <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Lorem Ipsum"
-                  position="DSC Member"
-                />
+                <Member img={val.img} name={val.name} position={val.position} />
               </Grid>
             ))}
           </Grid>
@@ -270,13 +231,9 @@ const photoTeam=[
             justify="space-around"
             alignItems="center"
           >
-            {[1, 6, 5, 7, 2, 3, 7, 9].map(val => (
+            {managementTeam.map(val => (
               <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Lorem Ipsum"
-                  position="DSC Member"
-                />
+                <Member img={val.img} name={val.name} position={val.position} />
               </Grid>
             ))}
           </Grid>
@@ -301,13 +258,9 @@ const photoTeam=[
             justify="space-around"
             alignItems="center"
           >
-            {[1, 2, 9].map(val => (
+            {contentTeam.map(val => (
               <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Lorem Ipsum"
-                  position="DSC Member"
-                />
+                <Member img={val.img} name={val.name} position={val.position} />
               </Grid>
             ))}
           </Grid>
@@ -331,13 +284,9 @@ const photoTeam=[
             justify="space-around"
             alignItems="center"
           >
-            {[7, 9].map(val => (
+            {photoTeam.map(val => (
               <Grid>
-                <Member
-                  img={profileIcon}
-                  name="Lorem Ipsum"
-                  position="DSC Member"
-                />
+                <Member img={val.img} name={val.name} position={val.position} />
               </Grid>
             ))}
           </Grid>
@@ -363,13 +312,9 @@ const photoTeam=[
           justify="space-around"
           alignItems="center"
         >
-          {[1, 2, 3, 7, 9].map(val => (
+          {socialMedia.map(val => (
             <Grid>
-              <Member
-                img={profileIcon}
-                name="Tanisha Srivastava"
-                position="DSC Member"
-              />
+              <Member img={val.img} name={val.name} position={val.position} />
             </Grid>
           ))}
         </Grid>
