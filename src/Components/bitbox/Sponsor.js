8d5d1b0cbbed8269a74f64cf71d379d8b8f2f5db
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2021-02-08 18:20:21 +0530

    sponsors-added

--- src/Components/bitbox/Sponsor.js
+++ src/Components/bitbox/Sponsor.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Button, Container, Box } from '@material-ui/core';
+import { Typography, Grid, Container, Box } from '@material-ui/core';
 import { Link, useHistory } from 'react-router-dom';
 import SponsorCard from './SponsorCard';
 
@@ -39,11 +39,34 @@ export default function Sponsor(props) {
       >
         Co-Sponsors
       </Typography>
-      <div className="sponsor-container">
+      <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+      >
         <SponsorCard key="0" src={require('../../images/sponsors/matic-light.png')} link="https://matic.network" />
         <SponsorCard key="1" src={require('../../images/sponsors/tezos-light.png')} link="https://tezos.com" />
         <SponsorCard key="2" src={require('../../images/sponsors/portis-light.png')} link="https://www.portis.io/" />
-      </div>
+        <SponsorCard key="2" src={require('../../images/sponsors/GMC_logo.png')} link="http://givemycertificate.com/" />
+      </Grid>
+      <Typography
+        data-aos="flip-down"
+        variant='h5'
+        component='p'
+        className='dsc-font'
+        align='center'
+      >
+        Sponsors
+      </Typography>
+      <Grid
+          container
+          direction="row"
+          justify="space-around"
+          alignItems="center"
+      >
+        <SponsorCard key="0" src={require('../../images/sponsors/taskade-meta.png')} link="https://www.taskade.com/" />
+      </Grid>
     </div>
   );
 }
