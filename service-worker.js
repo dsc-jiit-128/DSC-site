a9124c75ef3a8f364c9640cf96ff27d67fb993d5
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2021-03-01 21:39:12 +0530

    updated

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f7002a83d8148564d8ab870f38dfbd4b.js"
+  "./precache-manifest.bfef0483f7e89e1afef4d174dd44f3b2.js"
 );
 
 self.addEventListener('message', (event) => {
