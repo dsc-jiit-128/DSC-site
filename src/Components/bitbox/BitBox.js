38270480371666031380852f05f9405e0342774d
Hardikag17 <hardikag17@gmail.com>
2022-01-27 14:43:17 +0530

    bus fixed

--- src/Components/bitbox/BitBox.js
+++ src/Components/bitbox/BitBox.js
@@ -1,6 +1,5 @@
 import { Typography, Container, Box } from '@material-ui/core';
 import React, { useEffect } from 'react';
-import { useHistory } from 'react-dom';
 import Kaleidoscope from 'ak-kaleidoscope';
 import Section4 from '../landing-page/Section4';
 import Theme from './Theme';
@@ -10,7 +9,6 @@ import './bitbox.scss';
 import './bitbox2.scss';
 
 export default function BitBox({ isDark }) {
-  const history = useHistory();
   useEffect(() => {
     new Kaleidoscope({
       selector: '#kaleidoscope',
@@ -21,8 +19,64 @@ export default function BitBox({ isDark }) {
     });
   }, []);
   return (
-    <div className='bitbox' id='hero-section' style={{ height: '100vh' }}>
-      <canvas id='kaleidoscope'></canvas>
+    <div id='hero-section' style={{ height: '100vh' }}>
+      <center>
+        <canvas id='kaleidoscope'></canvas>
+        <Container fixed className='hero-container'>
+          <Typography
+            variant='h1'
+            component='h1'
+            align='center'
+            className='box-font margintopmax'
+            style={isDark ? { color: '#fff' } : { color: '#000' }}>
+            BITBOX 2.0
+          </Typography>
+
+          <div style={isDark ? { color: '#fff' } : { color: '#000' }}>
+            Google Developer Student Club JIIT-128 is pleased to announce that
+            we are hosting our annual tech hackathon "BIT BOX" in the month of
+            march for the students intrested, all over the world.
+          </div>
+          <button className='bitbox1'>Bit Box 2020 Memories</button>
+        </Container>
+        <Theme />
+        <SponsorUs />
+
+        <div>
+          <div className='heading'>
+            <h1>
+              {' '}
+              <u>Sessions</u>
+            </h1>
+            Yet to be announced
+          </div>
+          <div className='heading'>
+            <h1>
+              {' '}
+              <u>Speakers and Judges</u>
+            </h1>
+            To be announced
+          </div>
+          <div className='heading'>
+            <h1>
+              <u>Prizes</u>
+            </h1>
+            To be announced
+          </div>
+          <div className='heading'>
+            <h1>
+              {' '}
+              <u>Community Partners</u>
+            </h1>
+            To be announced
+          </div>
+        </div>
+      </center>
+      <FAQ />
+      <Container id='contact'>
+        <Section4 isDark={isDark} />
+      </Container>
+      {/*
       <Container fixed className='hero-container'>
         <Typography
           variant='h1'
@@ -32,35 +86,10 @@ export default function BitBox({ isDark }) {
           align='center'>
           BITBOX 2.0
         </Typography>
-        <div>
-          Google Developer Student Club JIIT-128 is pleased to announce that we
-          are hosting our annual tech hackathon "BIT BOX" in the month of march
-          for the students intrested, all over the world.
-        </div>
-        <button className='bitbox1' onClick={history.push('#/bitbox1')}>
-          Bitbox 2020 Memories
-        </button>
+        
       </Container>
-      <Theme />
-      <SponsorUs />
-      <div>
-        <div>
-          <u>Sessions</u>
-        </div>
-        <div>
-          <u>Speakers and Judges</u>
-        </div>
-        <div>
-          <u>Prizes</u>
-        </div>
-        <div>
-          <u>Community Partners</u>
-        </div>
-      </div>
-      <FAQ />
-      <Container id='contact'>
-        <Section4 isDark={isDark} />
-      </Container>
+      
+    {' '} */}
     </div>
   );
 }
