9b32e1c19e3f75ae02b955c35468e1ff4d9b4c36
sarthakpant772 <sarthak.pant31@gmail.com>
2022-02-06 13:09:42 +0530

    added speakers

--- src/Components/bitbox/BitBox.js
+++ src/Components/bitbox/BitBox.js
@@ -6,7 +6,7 @@ import Kaleidoscope from 'ak-kaleidoscope';
 import Section4 from '../landing-page/Section4';
 import Theme from './Theme';
 import SponsorUs from './SponsorUs';
-import Sessions from './bitbox2/Sessions';
+import Speakers from './bitbox2/Speakers';
 import FAQ from './FAQ';
 import './bitbox.scss';
 import './bitbox2.scss';
@@ -32,20 +32,21 @@ export default function BitBox({ isDark }) {
     };
   }, []);
   return (
-    <div id='hero-section' style={{ height: '100vh' }}>
+    <div id="hero-section" style={{ height: "100vh" }}>
       <center>
-        <canvas id='kaleidoscope'></canvas>
-        <Container fixed className='hero-container'>
+        <canvas id="kaleidoscope"></canvas>
+        <Container fixed className="hero-container">
           <Typography
-            variant='h1'
-            component='h1'
-            align='center'
-            className='box-font margintopmax'
-            style={isDark ? { color: '#fff' } : { color: '#292929' }}>
+            variant="h1"
+            component="h1"
+            align="center"
+            className="box-font margintopmax"
+            style={isDark ? { color: "#fff" } : { color: "#292929" }}
+          >
             BITBOX 2.0
           </Typography>
 
-          <div style={isDark ? { color: '#fff' } : { color: '#000' }}>
+          <div style={isDark ? { color: "#fff" } : { color: "#000" }}>
             <h4>
               Google Developer Student Club JIIT-128 is pleased to announce that
               we are hosting our annual tech hackathon "BIT BOX" from 4th - 6th
@@ -58,72 +59,79 @@ export default function BitBox({ isDark }) {
               devfolio button below!!
             </h4>
           </div>
-          <div className='hero-btn-container'>
+          <div className="hero-btn-container">
             <Box
               style={{
-                height: '100px',
-                width: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column',
-              }}>
+                height: "100px",
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                flexDirection: "column",
+              }}
+            >
               <div
-                className='apply-button'
-                data-hackathon-slug='bitbox-2'
-                data-button-theme='light'
-                style={{ height: '44px', width: '312px' }}
+                className="apply-button"
+                data-hackathon-slug="bitbox-2"
+                data-button-theme="light"
+                style={{ height: "44px", width: "312px" }}
               />
             </Box>
           </div>
         </Container>
         <div>
           <div
-            className='frameContainer'
-            style={isDark ? { color: '#fff' } : { color: '#000' }}>
-            <div className='frameContainer'>
+            className="frameContainer"
+            style={isDark ? { color: "#fff" } : { color: "#000" }}
+          >
+            <div className="frameContainer">
               <iframe
-                className='frameCard'
-                src='https://www.youtube.com/embed/XJ7E5l2YfB0'
-                title='YouTube video player'
-                frameborder='1'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                allowfullscreen></iframe>
+                className="frameCard"
+                src="https://www.youtube.com/embed/XJ7E5l2YfB0"
+                title="YouTube video player"
+                frameborder="1"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
-            <div style={{ marginTop: '50px' }}></div>
+            <div style={{ marginTop: "50px" }}></div>
           </div>
 
-          <div className='heading'>
+          <div className="heading">
             <Typography
-              variant='p'
-              align='center'
-              component='p'
-              className='dsc-font'
-              data-aos='flip-left'
-              style={{ paddingTop: '4rem' }}>
+              variant="p"
+              align="center"
+              component="p"
+              className="dsc-font"
+              data-aos="flip-left"
+              style={{ paddingTop: "4rem" }}
+            >
               <Schedule isDark={isDark} />
             </Typography>
           </div>
-          <div className='heading'>
+          <div className="heading">
             <Typography
-              variant='h3'
-              align='center'
-              component='p'
-              className='dsc-font'
-              data-aos='flip-left'
-              style={{ paddingTop: '4rem' }}>
+              variant="h3"
+              align="center"
+              component="p"
+              className="dsc-font"
+              data-aos="flip-left"
+              style={{ paddingTop: "4rem" }}
+            >
               Speakers and Judges
+              <Speakers />
             </Typography>
-            To be announced
           </div>
-          <div className='heading'>
+            <h3 style={{marginTop:"10px"}}>More exciting sessions coming soon. Stay tuned!!</h3>
+          <div className="heading">
             <Typography
-              variant='h3'
-              align='center'
-              component='p'
-              className='dsc-font'
-              data-aos='flip-left'
-              style={{ paddingTop: '4rem' }}>
+              variant="h3"
+              align="center"
+              component="p"
+              className="dsc-font"
+              data-aos="flip-left"
+              style={{ paddingTop: "4rem" }}
+            >
               Prizes
             </Typography>
             To be announced
@@ -137,23 +145,10 @@ export default function BitBox({ isDark }) {
         <SponsorUs isDark={isDark} />
       </center>
       <FAQ />
-      <Container id='contact'>
+      <Container id="contact">
         <Section4 isDark={isDark} />
       </Container>
-      {/*
-      <Container fixed className='hero-container'>
-        <Typography
-          variant='h1'
-          component='h1'
-          className='box-font margintopmax'
-          style={isDark ? { color: '#fff' } : {}}
-          align='center'>
-          BITBOX 2.0
-        </Typography>
-        
-      </Container>
-      
-    {' '} */}
+
       {/* <button
         className="bitbox1"
         onClick={() => {
---
+++ src/Components/bitbox/bitbox2/Speakers.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Typography, Container, Box, Grid, Button } from "@material-ui/core";
+import Member from "./MemberCard";
+// import profileIcon from "../../images/profileAvatar.jpg";
+// import Img from "../import_image/importImage.js";
+
+const Speaker = ({ dark }) => {
+  const speakerArr = [
+    {
+      img: "AyushSing.png",
+      social: "https://www.linkedin.com/in/alphaayush",
+    },
+    {
+      img: "RangaK .png",
+      social: "https://www.linkedin.com",
+    },
+  ];
+
+  return (
+    <div>
+      <Container className="mt-9">
+        <Box className="team-sections">
+          <Typography
+            variant="h3"
+            align="center"
+            component="p"
+            className="dsc-font"
+            data-aos="flip-left"
+          >
+            <span>Our Speakers</span>
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            justify="space-around"
+            alignItems="center"
+            className="marginTop padB"
+            spacing={10}
+          >
+            {speakerArr.map((val) => (
+              <Grid className="speaker-grid" data-aos="zoom-out">
+                <Member img={val.img} social={val.social} dark={dark} />
+              </Grid>
+            ))}
+            {/* <Typography
+                        variant="h5"
+                        align="center"
+                        component="p"
+                        className="dsc-font"
+                        data-aos="flip-left"
+                        style={{padding: "50px 0px"}}
+                        >
+                            Coming Soon
+                        </Typography> */}
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default Speaker;
